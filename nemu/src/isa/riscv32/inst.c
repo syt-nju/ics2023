061d7dc18a74f26cc63a45f0f276125f7e3a6f2d
tracer-ics2023 <tracer@njuics.org>
2024-01-20 17:28:43 +0800

    >  compile NEMU 221300094 沈宇腾 Linux steven-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:28:43 up  1:03,  1 user,  load average: 0.37, 0.34, 0.30

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -48,6 +48,8 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_J:                   immJ();break;
     case TYPE_B: src1R(); src2R(); immB(); break;
+    case TYPE_R: src1R(); src2R();         break;
+
   }
 }
 
