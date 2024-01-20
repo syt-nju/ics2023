6f753b83f6a061c829c60d3b081e6cfb82267fb3
tracer-ics2023 <tracer@njuics.org>
2024-01-20 19:19:24 +0800

    >  compile NEMU 221300094 沈宇腾 Linux steven-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:19:24 up  2:54,  1 user,  load average: 0.18, 0.19, 0.18

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -30,7 +30,7 @@ enum {
 #define src1R() do { *src1 = R(rs1); } while (0)
 #define src2R() do { *src2 = R(rs2); } while (0)
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
-#define immU() do { *imm = (SEXT(BITS(i, 31, 12), 20) << 12)&(4294967295); } while(0)//modified
+#define immU() do { *imm = (SEXT(BITS(i, 31, 12), 20) << 12); } while(0)//modified
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 
 
@@ -93,7 +93,7 @@ static int decode_exec(Decode *s) {
 //to solve fact
   INSTPAT("0000001 ????? ????? 000 ????? 01100 11", mul    , R, R(rd) = src1 * src2);
 //to solve string
-  INSTPAT("??????? ????? ????? ??? ????? 01101 11", lui    , U, R(rd) = imm;printf("imm:%u\n",imm);); 
+  INSTPAT("??????? ????? ????? ??? ????? 01101 11", lui    , U, R(rd) += imm;printf("imm:%u\n",imm);); 
 
 
 //end modification by syt
